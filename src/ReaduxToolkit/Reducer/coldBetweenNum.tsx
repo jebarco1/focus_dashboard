f8497ccd997bd34eedc9cd0aeeacbe1f34eec190
jebarco <jebarco@gmail.com>
2025-01-05 23:45:53 -0500

    fix another api call issue

--- src/ReaduxToolkit/Reducer/coldBetweenNum.tsx
+++ src/ReaduxToolkit/Reducer/coldBetweenNum.tsx
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { RootState } from "../Store" // Import your RootState type
+
 
 // Define the interface for the state
 interface PatternsState {
@@ -18,10 +20,13 @@ const initialState: PatternsState = {
 // Define an async thunk for the API call
 export const coldBetweenNumPatterns = createAsyncThunk(
   'ColdBetweenNum/coldBetweenNumPatterns',
-  async (payload: { numbers: number[]; power: number }, { rejectWithValue }) => {
+  async (payload: { numbers: number[]; power: number }, { getState, rejectWithValue }) => {
     try {
+
+      const state = getState() as RootState; // Access the Redux state
+      const lotterySelect = state.lotterySelect.value; // Get the lotterySelect value
       const numbersString = payload.numbers.join(',');
-      const url = `http://localhost:8080/jankgo/metricController/coldBetweenNum/['powerball']`;
+      const url = `http://localhost:8080/jankgo/metricController/coldBetweenNum/[%22${lotterySelect}%22]`;
       const response = await axios.get(url);
       return response.data;
 
